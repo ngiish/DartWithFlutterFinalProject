ea7acd71af63a0e83f6a985a043d3db6fd19e79d
Isaac <govegeku00@gmail.com>
2024-06-14 18:33:22 +0300

    Creation of files to Handle events lists and definition of the Event class

---
+++ lib/event.dart
@@ -0,0 +1,8 @@
+//Defines the Event class with a title and date
+//Defining event model
+class Event {
+  final String title;
+  final DateTime date;
+
+  Event({required this.title, required this.date});
+}
---
+++ lib/event_provider.dart
@@ -0,0 +1,22 @@
+//Manages the state of the events list, including adding and removing events
+//Setting up state management with provider
+import 'package:flutter/material.dart';
+import 'event.dart';
+
+
+class EventProvider with ChangeNotifier {
+  final List<Event> _events = [];
+
+  List<Event> get events => _events;
+
+
+  void addEvent(Event event) {
+    _events.add(event);
+    notifyListeners();
+  }
+
+  void removeEvent(Event event) {
+    _events.remove(event);
+    notifyListeners();
+  }
+}
